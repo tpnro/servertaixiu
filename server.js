b7bdd8e171ffaeea50dcde6a0c9331b978caa534
tpnro <td27dpbgwv@privaterelay.appleid.com>
2026-04-22 18:42:43 +0700

    TELE : @PHUCCODER14

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var io = socketIO(server,
 });
 var cuoc   = Array();
 var system = {
-	url : 'http://localhost:8080', 
+	url : 'https://babyhost.online/', 
 	keycode : '123456', 
 	ngocrong_min : 1000, 
 	ngocrong_max : 2000,
